a528b326e6e8f52d6290b9e25dc63ba9d52bdb47
rowaxl <rowaxl0@gmail.com>
2021-03-27 16:05:27 +0900

    update sw-cache

--- next.config.js
+++ next.config.js
@@ -1,33 +1,25 @@
-const path = require('path')
-const SWPrecache = require('sw-precache-webpack-plugin')
+const NextWrokboxWebpack = require('next-workbox-webpack-plugin')
 
 module.exports = {
-  webpack: (config, { dev }) => {
-    const oldEntry = config.entry
+  webpack: (config, { isServer, dev, buildId, config: { distDir } }) => {
 
-    config.entry = () => oldEntry().then(entry => {
-      if (entry['main.js']) entry['main.js'].push(path.resolve('./utils/offline'))
-
-      return entry
-    })
-
-    if (!dev) {
-      config.plugins.push(new SWPrecache({
-        cacheId: 'lighthouse-scored',
-        filepath: path.resolve('./static/sw.js'),
-        staticFileGlobs: ['static/**/*'],
-        minify: true,
-        staticFileGlobsIgnorePatterns: [/\.next\//],
-        runtimeCaching: [{
-          handler: 'fastest',
-          urlPattern: /[.](png|jpg|css)/
-        }, {
-          handler: 'networkFirst',
-          urlPattern: /^http.*/
-        }]
-      }))
+    if (!isServer && !dev && config.plugin) {
+      config.plugin.push(
+        new NextWrokboxWebpack({
+          importWorkboxFrom: 'cdn',
+          distDir,
+          buildId,
+          swDestRoot: './static/sw.js',
+          swURLRoot: '/static/sw.js'
+        })
+      )
     }
 
     return config
+  },
+  exportPathMap: () => {
+    return {
+      '/': { page: '/' }
+    }
   }
 }
