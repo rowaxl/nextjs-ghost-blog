338ce91cba39a67ae1b52b61e4ec1a33fb2b0573
rowaxl <rowaxl0@gmail.com>
2021-03-27 16:39:13 +0900

    update manifext

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
-const NextWrokboxWebpack = require('next-workbox-webpack-plugin')
+const NextWorkboxWebpack = require('next-workbox-webpack-plugin')
 
 module.exports = {
   webpack: (config, { isServer, dev, buildId, config: { distDir } }) => {
 
-    if (!isServer && !dev && config.plugin) {
-      config.plugin.push(
-        new NextWrokboxWebpack({
-          importWorkboxFrom: 'cdn',
+    if (!isServer && !dev) {
+      config.plugins.push(
+        new NextWorkboxWebpack({
           distDir,
           buildId,
-          swDestRoot: './static/sw.js',
-          swURLRoot: '/static/sw.js'
         })
       )
     }
